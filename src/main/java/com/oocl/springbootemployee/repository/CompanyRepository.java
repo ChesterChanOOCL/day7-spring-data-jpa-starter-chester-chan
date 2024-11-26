2065e4fdbfc3fd152901055b7aab58b356c95798
CHANCH10 <chester.chan@oocl.com>
2024-11-26 22:31:35 +0800

    feat: CompanyRepository interface

--- src/main/java/com/oocl/springbootemployee/repository/CompanyRepository.java
+++ src/main/java/com/oocl/springbootemployee/repository/CompanyRepository.java
@@ -1,14 +1,13 @@
-//package com.oocl.springbootemployee.repository;
-//
-//import com.oocl.springbootemployee.model.Company;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface CompanyRepository extends JpaRepository<Company, Integer> {
-//
-//    Company getCompanyById(Integer id);
-//
-//    //get size
-//    long count();
-//}
+package com.oocl.springbootemployee.repository;
+import com.oocl.springbootemployee.model.Company;
+
+import com.oocl.springbootemployee.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Integer>{
+    Employee getCompanyById(Integer id);
+
+    //get size
+    long count();
+}
