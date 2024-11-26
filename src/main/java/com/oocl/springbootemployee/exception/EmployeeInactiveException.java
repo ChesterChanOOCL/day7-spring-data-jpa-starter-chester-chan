eb95b0db6f5c713cb6b561a8aa81d465ecb35e7d
CHANCH10 <chester.chan@oocl.com>
2024-11-26 17:58:51 +0800

    feat: handling Exception by Class

--- src/main/java/com/oocl/springbootemployee/exception/EmployeeInactiveException.java
+++ src/main/java/com/oocl/springbootemployee/exception/EmployeeInactiveException.java
@@ -1,4 +1,7 @@
 package com.oocl.springbootemployee.exception;
 
 public class EmployeeInactiveException extends RuntimeException {
+    public EmployeeInactiveException(String employeeNotFound) {
+
+    }
 }
